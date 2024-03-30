9ef610e107f07c415661ad9703fa4ad465e174f8
noidvan <i.lin@mail.utoronto.ca>
2024-03-30 15:55:19 -0400

    Add text to speech function.

--- image_to_speech.py
+++ image_to_speech.py
@@ -1,11 +1,13 @@
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part, Image
 from google.oauth2 import service_account
+from google.cloud import texttospeech
+from playsound import playsound
 
 
 def generate_text(project_id: str, location: str) -> str:
     # Initialize Vertex AI
-    credentials = service_account.Credentials.from_service_account_file('service_acc_key.json')
+    
     vertexai.init(project=project_id, location=location, credentials=credentials)
     # Load the model
     img = Image.load_from_file("WechatIMG19.jpg")
@@ -15,5 +17,36 @@ def generate_text(project_id: str, location: str) -> str:
     print(response.text)
     return response.text
 
+def text_to_speech(text):
+    """Synthesizes speech from the input string of text."""
+
+    client = texttospeech.TextToSpeechClient(credentials=credentials)
+
+    input_text = texttospeech.SynthesisInput(text=text)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US",
+        name="en-US-Standard-C",
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        request={"input": input_text, "voice": voice, "audio_config": audio_config}
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+    playsound('output.mp3')
+
 if __name__ == "__main__":
-    generate_text("avian-principle-418814", 'us-central1')
+    credentials = service_account.Credentials.from_service_account_file('service_acc_key.json')
+    res = generate_text("avian-principle-418814", 'us-central1')
+    text_to_speech(res)
