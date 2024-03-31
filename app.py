8c2775373deabccb0a241cf49bc942ae5c49f0ba
Jacqueline <82245936+Jacqueline2727@users.noreply.github.com>
2024-03-31 00:40:30 -0400

    Add Safety Functions

--- app.py
+++ app.py
@@ -20,11 +20,8 @@ def upload_image():
         image = Image.open(BytesIO(image_data))
         image.save('uploaded_photo.png', 'PNG')  # Save the photo to a file
         myimg = its.Image.load_from_file("uploaded_photo.png")
-        #res = its.generate_text(myimg)
-        #its.text_to_speech(res)
-        additional_question = "Do you need more detailed description of the picture?"
-        its.text_to_speech(additional_question)
-
+        res = its.generate_text(myimg)
+        its.text_to_speech(res)
 
         return 'Image uploaded successfully!'
     else:
@@ -33,6 +30,24 @@ def upload_image():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+def ask_user():
+    # Your Python function logic here
+    print("Python function called")
+
+@app.route('/ask_user', methods=['GET'])
+def call_python_function():
+    ask_user()
+    return 'Python function called successfully'
+
+def ask_user2():
+    # Your Python function logic here
+    print("Python function 2 called")
+
+@app.route('/ask_user2', methods=['GET'])
+def call_python_function2():
+    ask_user2()
+    return 'Python function 2 called successfully'
 
 if __name__ == '__main__':
     its.setup()
